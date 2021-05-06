41614d9c005f93da9ef485a9c4923dbc81b5d056
Abhishek <ABHISHEKKUMAR998>
2021-05-06 12:37:24 +0530

    Adding Nodes

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,21 +1,49 @@
 package LinkedList;
 
 public class LinkedList {
-	Node head; 					
-	static class Node { 
-        int data; 
-        Node next; 
-        Node(int d) { 
-        	data = d; 
-            next = null; 
-        } 						
-    } 
-    public static void main(String[] args) { 
-        LinkedList listObj = new LinkedList(); 
-        listObj.head = new Node(56); 
-        Node second = new Node(30); 
-        Node third = new Node(70); 
-        listObj.head.next = second;  
-        second.next = third; 
-    } 
-}
+	public INode head;
+	public INode tail;
+
+	public LinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void add(INode newNode) {
+		if (this.tail == null) {
+			this.tail = newNode;
+		}
+		if (this.head == null) {
+			this.head = newNode;
+		} else {
+			INode tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	public void printMyNode() {
+		StringBuffer myNodes = new StringBuffer("My Nodes: ");
+		INode tempNode = head;
+		while (tempNode.getNext() != null) {
+			myNodes.append(tempNode.getKey());
+			if (!tempNode.equals(tail))
+				myNodes.append("->");
+			tempNode = tempNode.getNext();
+		}
+		myNodes.append(tempNode.getKey());
+		System.out.println(myNodes);
+	}
+	public static void main(String [] args) {
+		
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		LinkedList myLinkedList = new LinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThirdNode);
+		myLinkedList.printMyNode();
+		
+	}
+} 
